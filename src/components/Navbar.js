62d99574262ef4a6ede965317110355d0d0214de
AnichoKraus <anichokraus3123@gmail.com>
2023-05-16 06:05:10 -0700

    'second'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../style/home.css';
+import { Link } from 'react-router-dom';
 
 class Navbar extends React.Component {
   render() {
@@ -34,6 +35,7 @@ class Navbar extends React.Component {
                 <button type="button" aria-label="Theme switcher" data-title="Toggle dark mode" className="blog-theme-switcher tooltip-handle css-dsq3q4" style={{marginRight: '25px',backgroundColor:'#9e9fa1'}}>
                     <svg className="css-edyqgg" id='_dL' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path d="M3 11.4489C3 16.7238 7.16904 21 12.3118 21C16.2709 21 19.6529 18.4657 21 14.8925C19.9331 15.4065 18.7418 15.6938 17.485 15.6938C12.9137 15.6938 9.20787 11.8928 9.20787 7.20396C9.20787 5.24299 9.85605 3.4373 10.9446 2C6.45002 2.6783 3 6.65034 3 11.4489Z" ></path></svg>
                 </button>
+                <Link to="#" className="btn  waves-effect waves-light _connect">Connect Wallet</Link>
             </div>
         </nav>
   	);
